598e6aea53ebf9e41c6e9fb5c54ed0728b826212
simofleanta <simo.fleanta@gmail.com>
2021-05-31 15:38:12 +0200

    Create book_talk_app.js

---
+++ book_talk_app.js
@@ -0,0 +1,18 @@
+
+        
+        <div id="Connect"><h1 class="section-title">Book a meeting</h1></div>
+        <div
+          class="calendly-inline-widget"
+          data-url="https://calendly.com/simo-fleanta"
+          style="min-width: 320px; height: 500px"
+        ></div>
+
+
+        <script src="app.js"></script>
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async
+        ></script>      
+
+        
